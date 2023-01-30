6b04ab253f3d0430745bf22ffb59aaac6873ce52
deefi <dee7ine@gmail.com>
2023-01-30 20:29:28 +0100

    Added random articles and title export

--- wiki/articles.py
+++ wiki/articles.py
@@ -16,21 +16,23 @@ class WikiContentParser:
 
     def __init__(self) -> None:
         
-        article_text = self.get_article_text(title='Belgian Ship A4')
-        article_text = self.clean_text(text=article_text)
+        # article_text = self.get_article_text(title='Belgian Ship A4')
+        article_text = self.get_article_text(title=self.get_random_article_title())
+        article_text = self.clean_text(text=article_text, clear_new_lines=False)
         print(article_text)
 
     @staticmethod
     def get_article_list() -> None:
+        
         session = requests.Session()
         response = session.get(url=API_URL, params=PARAMS)
         
-        DATA = response.json()
+        data = response.json()
         
-        RANDOMS = DATA["query"]["random"]
+        random_articles = data["query"]["random"]
         
-        for r in RANDOMS:
-            print(r["title"])
+        for article in random_articles:
+            print(article["title"])
 
     @DeprecationWarning
     def random_article(self) -> None:
@@ -39,26 +41,31 @@ class WikiContentParser:
         print(page)
 
     @staticmethod
-    def get_random_article_title() -> None:
+    def get_random_article_title() -> str:
+        
         url = requests.get('https://en.wikipedia.org/wiki/Special:Random')
         soup = BeautifulSoup(url.content, 'html.parser')
         title = soup.find(class_='firstHeading').text
         text = soup.p.text
         print(f'Article title: {title}')
         print(f'Article text: {text}')
+        
+        return title 
 
     @staticmethod
     def get_article_text(title: str) -> str:
+        
         wiki = wikipedia.page(title)  # 'Belgian Ship A4'
         text_content = wiki.content
         
         return text_content
 
     @staticmethod
-    def clean_text(text: str) -> str:
+    def clean_text(text: str, clear_new_lines: bool) -> str:
         
         text = re.sub(r'==.*?==+', '', text)
-        #  text = text.replace('\n', '')
+        
+        if clear_new_lines: text = text.replace('\n', '')
         
         return text
 
