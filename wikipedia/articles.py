721e8d1c6b172b048f263c000d37350aa748aaef
deefi <dee7ine@gmail.com>
2023-01-30 19:22:32 +0100

    refactored article.py

--- wikipedia/articles.py
+++ wikipedia/articles.py
@@ -11,50 +11,61 @@ PARAMS = {
     "rnlimit": "5"
 }
 
-def get_article_list() -> None:
-    session = requests.Session()
-    response = session.get(url=API_URL, params=PARAMS)
+class Wiki_Content_Parser():
     
-    DATA = response.json()
+    def __init__(self):
+        
+        article_text = self.get_article_text(title='Belgian Ship A4')
+        article_text = self.clean_text(text=article_text)
+        print(article_text)
     
-    RANDOMS = DATA["query"]["random"]
-    
-    for r in RANDOMS:
-        print(r["title"])
+    def get_article_list(self) -> None:
+        session = requests.Session()
+        response = session.get(url=API_URL, params=PARAMS)
+        
+        DATA = response.json()
+        
+        RANDOMS = DATA["query"]["random"]
+        
+        for r in RANDOMS:
+            print(r["title"])
 
-@DeprecationWarning
-def random_article() -> None:
-    """
-    Get random wikipedia article
-    
-    :return 
-    """
-    
-    page = requests.get("https://en.wikipedia.org/api/rest_v1/page/random/summary").json()
-    print(page)
-    
-def get_random_article_title() -> None:
-    url = requests.get('https://en.wikipedia.org/wiki/Special:Random')
-    soup = BeautifulSoup(url.content, 'html.parser')
-    title = soup.find(class_='firstHeading').text
-    text = soup.p.text
-    print(f'Article title: {title}')
-    print(f'Article text: {text}')
-    
-def get_article_text() -> None:
-    wiki = wikipedia.page('Belgian Ship A4')
-    text = wiki.content
-    
-    #cleaning text
-    text = re.sub(r'==.*?==+', '', text)
-    text = text.replace('\n', '')
-    print(text)
-    print(type(text))
+    @DeprecationWarning
+    def random_article(self) -> None:
+        """
+        Get random wikipedia article
+        
+        :return 
+        """
+        
+        page = requests.get("https://en.wikipedia.org/api/rest_v1/page/random/summary").json()
+        print(page)
+        
+    def get_random_article_title(self) -> None:
+        url = requests.get('https://en.wikipedia.org/wiki/Special:Random')
+        soup = BeautifulSoup(url.content, 'html.parser')
+        title = soup.find(class_='firstHeading').text
+        text = soup.p.text
+        print(f'Article title: {title}')
+        print(f'Article text: {text}')
+        
+    def get_article_text(self, title: str) -> str:
+        wiki = wikipedia.page(title) # 'Belgian Ship A4'
+        text_content = wiki.content
+        
+        return text_content
+        
+    def clean_text(self, text: str) -> None:
+        
+        text = re.sub(r'==.*?==+', '', text)
+        text = text.replace('\n', '')
+        
+        return text
+        
+        
     
       
 if __name__ == "__main__":
-    # get_random_article()
-    # get_article_list()
-    get_article_text()
     
+    content_parser = Wiki_Content_Parser()
     
