91e38716cdb46a7c795980b1c364307aabb4b1ef
deefi <dee7ine@gmail.com>
2023-02-16 22:19:19 +0100

    dee7ine: Minor improvements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import __future__
+from __future__ import annotations
 
 __version__ = '0.1'
 __author__ = 'Bartlomiej Jargut'
@@ -15,51 +15,51 @@ from wiki.articles import WikiArticleParser
 from wiki.titles import Titles
 
 
-HTML_TEMPLATE = NewType('HTML_TEMPLATE', 'str')
-ERROR_CODE = NewType('ERROR_CODE', 'int')
-
 app = Flask(__name__) 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 parser = WikiArticleParser()
-titles_list = Titles.all_titles
-article_title = random.choice(titles_list)
+article_title: str = random.choice(Titles.all_titles)
 article_text = parser.get_article_text(article_title, print_content=True)
 
 
 @app.route('/')
-def index() -> HTML_TEMPLATE:
+def index() -> str:
     return render_template('index.html', title=article_title, content=article_text)
+
 
 @app.route('/', methods=['POST'])
 def get_guess() -> str:
     text = request.form['text']
     return text
 
+
 @app.route('/user/<name>')
-def user(name: str) -> HTML_TEMPLATE:
+def user(name: str) -> str:
     return render_template('user.html', name=name, content=f'{article_title}\n{article_text}')
 
+
 @app.route('/settings')
-def settings() -> HTML_TEMPLATE:
+def settings() -> str:
     return '<h1>Settings page<h1>'
+
 
 @app.route('/redirect')
 def zen_of_python() -> "Response":
     return redirect('https://peps.python.org/pep-0020/#the-zen-of-python')
 
+
 @app.errorhandler(404)
-def page_not_found(e: Exception) -> tuple[HTML_TEMPLATE, ERROR_CODE]:
+def page_not_found(e: Exception) -> tuple[str, int]:
     """Handler for Error 404.
-    
-    
     """    
     
     return render_template('404.html'), 400
 
+
 @app.errorhandler(500)
-def internal_server_error(e: Exception) -> tuple[HTML_TEMPLATE, ERROR_CODE]:
+def internal_server_error(e: Exception) -> tuple[str, int]:
     return render_template('500.html'), 500
 
 
--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,8 +1,23 @@
+import unittest
+from wiki.articles import WikiArticleParser
+from wiki.titles import Titles
+import random
 
-from wiki.articles import Wiki_Content_Parser
 
+class TestWikiArticleParser(unittest.TestCase):
 
-def setup():
+    def setup(self) -> tuple[WikiArticleParser, property]:
+        
+        parser = WikiArticleParser()
+        titles = Titles.all_titles
+        
+        return parser, titles
     
-    time.sleep(5)
-    content_parser = Wiki_Content_Parser()
+    def test_random_article(self) -> None:
+        parser, titles = self.setup()
+        
+        parser.get_article_text(random.choice(titles), print_content=True)
+
+
+if __name__ == '__main__':
+    unittest.main()
